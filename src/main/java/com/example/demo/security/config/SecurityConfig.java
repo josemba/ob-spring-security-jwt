b95d770647c597680b9a4c5e760c8df915bf7026
josemba <josemba@hotmail.com>
2022-10-14 21:24:19 +0200

    Cambio URL a permitir.

--- src/main/java/com/example/demo/security/config/SecurityConfig.java
+++ src/main/java/com/example/demo/security/config/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://angular-springboot-*.vercel.app"));
         configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:4200", "https://ob-angular-spring-ohhyq7js7-josemba.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
-        configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Origin", "X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
+        configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Origin", "X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization", "Access-Control-Allow-Methods", "Access-Control-Allow-Headers"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
